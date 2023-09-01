18130d249a38f6671589490407bbe1831db0d5ab
Bao Le <90585407+baotuanle@users.noreply.github.com>
2023-09-01 17:44:30 -0400

    Update firebase.js
    hiding

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAFTbNmbODW2uw3xvXSzwUreBLAiUQ8X4c",
-  authDomain: "messaing-app-4c788.firebaseapp.com",
-  projectId: "messaing-app-4c788",
-  storageBucket: "messaing-app-4c788.appspot.com",
-  messagingSenderId: "265740891315",
-  appId: "1:265740891315:web:4c0bb42bff2634b5e9acc6"
+//REMOVED
 };
 
 export const app = initializeApp(firebaseConfig);
